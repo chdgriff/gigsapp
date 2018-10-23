f2a487975b6acf29e1e8cc879d798fcc49db626d
Tony Man <tonyman316@gmail.com>
2018-10-23 15:20:40 -0700

    add login, sign up feature

--- gigsapp/app/assets/javascripts/application.js
+++ gigsapp/app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+
--- gigsapp/app/controllers/home_controller.rb
+++ gigsapp/app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
 	def index
-	end
+		
+  	end
+
+
 
 end
