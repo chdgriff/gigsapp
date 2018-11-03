2eedcb90da6c8bfa4ecbc4c5d6073ad4582339fd
Christopher Young <chtayoun@ucsc.edu>
2018-11-02 18:57:09 -0700

    added search bar

--- gigsapp/app/controllers/posts_controller.rb
+++ gigsapp/app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.where(["description LIKE ?","%#{params[:search]}%"])
   end
 
   # GET /posts/1
