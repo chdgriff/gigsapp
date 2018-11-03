9aba2750093a91a9068378389797cbee5acaa413
Christopher Young <chtayoun@ucsc.edu>
2018-11-03 12:04:19 -0700

    Revert "added search bar"
    This reverts commit 2eedcb90da6c8bfa4ecbc4c5d6073ad4582339fd.

--- gigsapp/app/controllers/posts_controller.rb
+++ gigsapp/app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.where(["description LIKE ?","%#{params[:search]}%"])
+    @posts = Post.all
   end
 
   # GET /posts/1
