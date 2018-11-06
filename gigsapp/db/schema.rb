c34f1d19c5c8cd7ecee96b17a9ae79400fb0b7c4
Chris Griffis <chrisiffis@gmail.com>
2018-11-05 17:34:43 -0800

    renamed post.body to post.description

--- gigsapp/db/schema.rb
+++ gigsapp/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_04_205315) do
+ActiveRecord::Schema.define(version: 2018_11_06_012913) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text "description"
     t.string "location"
     t.float "rate"
     t.datetime "created_at", null: false
