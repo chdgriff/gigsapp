9589c2e9b1d2e88953b25e595035ef57fee4b20e
Tom Bowen <tomb3326@gmail.com>
2018-11-11 17:30:49 -0800

    updated public page

--- gigsapp/app/controllers/posts_controller.rb
+++ gigsapp/app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.where(["description LIKE ?","%#{params[:search]}%"])
+    @posts_find = Post.all
   end
 
   # GET /posts/1
