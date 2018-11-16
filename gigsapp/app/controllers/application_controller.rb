ab677deba40d0bb095e0a83881e0a97ca4f9df85
Tangni Wang <twang63@ucsc.edu>
2018-11-16 14:19:46 -0800

    changed sign up page to only required details

--- gigsapp/app/controllers/application_controller.rb
+++ gigsapp/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :bio, :address,:address2, :city, :state, :zip, :employee, :employer, :age, :email, :password, :remember_me)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :age, :email, :password, :remember_me)}
     devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :remember_me)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :username, :bio, :address,:address2, :city, :state, :zip, :employee, :employer, :age, :email, :password, :current_password, :remember_me, :profile_image, :linkedin)}
 
