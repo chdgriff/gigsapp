38a639c878846e8f9c520d2de7a4a7cc1d69e18e
Chris Griffis <chrisiffis@gmail.com>
2018-11-20 15:41:52 -0800

    RE added profile image to migration

--- db/migrate/20181105002835_add_detail_to_user.rb
+++ db/migrate/20181105002835_add_detail_to_user.rb
@@ -12,5 +12,9 @@ class AddDetailToUser < ActiveRecord::Migration[5.2]
     add_column :users, :employee, :boolean
     add_column :users, :employer, :boolean
     add_column :users, :age, :boolean
+    add_column :users, :profile_image_file_name, :string
+    add_column :users, :profile_image_content_type, :string
+    add_column :users, :profile_image_file_size, :integer
+    add_column :users, :profile_image_updated_at, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_020407) do
+ActiveRecord::Schema.define(version: 2018_11_20_055921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
