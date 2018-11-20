12eef5b67b0e644aafe3a5fb9d625bf006e3aee6
Chris Griffis <chrisiffis@gmail.com>
2018-11-19 16:10:01 -0800

    Made gigsapp the root directory

--- gigsapp/app/models/job_application.rb
+++
@@ -1,4 +0,0 @@
-class JobApplication < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
-end
--- gigsapp/db/migrate/20181106012913_rename_body_to_description.rb
+++
@@ -1,5 +0,0 @@
-class RenameBodyToDescription < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :posts, :body, :description
-  end
-end
