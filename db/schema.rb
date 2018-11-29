036ff4d7bfcc14a08c45f6b344043b7bf2a3ea20
Chris Griffis <chrisiffis@gmail.com>
2018-11-28 16:29:50 -0800

    Generated model and controller search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_000202) do
+ActiveRecord::Schema.define(version: 2018_11_29_002842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 2018_11_21_000202) do
     t.integer "asset_file_size"
     t.datetime "asset_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "string"
+    t.string "location"
+    t.decimal "rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
