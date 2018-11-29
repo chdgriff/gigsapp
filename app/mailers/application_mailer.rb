9070365a2427c64edad67b059e23848f9965bc8c
Tom Bowen <tomb3326@gmail.com>
2018-11-28 20:39:05 -0800

    mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'postmaster@sandbox061518a02592439db918bdd969ec4f2f.mailgun.org'
   layout 'mailer'
+  @user = @post.user_id 
+  mail(to: @user.email, subject: 'you were accepted for the job')
 end
