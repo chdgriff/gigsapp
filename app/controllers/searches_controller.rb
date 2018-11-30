f05236623f6d9368f84b2d7c71af18fa14fee727
Christopher Young <chtayoun@ucsc.edu>
2018-11-30 14:53:07 -0800

    update

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,11 +4,16 @@ class SearchesController < ApplicationController
   end
 
   def create
-    @search = Search.create!(params[:search])
+    #@search = Search.create!(params[:search])
+    @search = Search.create(search_params)
     redirect_to @search
   end
 
   def show
     @search = Search.find(params[:id])
+  end
+  private
+  def search_params
+    params.require(:search).permit(:title, :description, :location, :rate)
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,15 +1,10 @@
 class Search < ApplicationRecord
-  def posts
-    @posts ||= find_posts
-  end
-
-  private
-
-  def find_posts
+  def serach_posts
+    posts = Post.all
     posts = posts.where("title like ?", "%#{title}%") if title.present?
     posts = posts.where("description like ?", "%#{description}%") if description.present?
     posts = posts.where("location ?", "%#{location}%") if location.present?
     posts = posts.where("rate = ?", rate) if rate.present?
-    posts
+    return posts
   end
 end
