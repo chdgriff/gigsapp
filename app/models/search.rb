59c63df2f5e5bea61a8d1b2c5feaee10677507f7
Christopher Young <chtayoun@ucsc.edu>
2018-11-30 15:08:24 -0800

    update

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,10 +1,10 @@
 class Search < ApplicationRecord
-  def serach_posts
+  def search_posts
     posts = Post.all
-    posts = posts.where("title like ?", "%#{title}%") if title.present?
-    posts = posts.where("description like ?", "%#{description}%") if description.present?
-    posts = posts.where("location ?", "%#{location}%") if location.present?
-    posts = posts.where("rate = ?", rate) if rate.present?
+    posts = posts.where(["title LIKE ?", "%#{title}%"]) if title.present?
+    posts = posts.where(["description LIKE ?", "%#{description}%"]) if description.present?
+    posts = posts.where(["location LIKE ?", "%#{location}%"]) if location.present?
+    posts = posts.where(["rate = ?", rate]) if rate.present?
     return posts
   end
 end
