9f8360a4ac94a9e3f1f41786a1dff130eb707592
Tom Bowen <tomb3326@gmail.com>
2018-11-30 16:03:46 -0800

    test

---
+++ app/mailers/model_mailer.rb
@@ -0,0 +1,13 @@
+class ModelMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.model_mailer.new_record_notification.subject
+  #
+  def new_record_notification
+    @greeting = "Hi"
+
+    mail to: "tcbowen@ucsc.edu"
+  end
+end
