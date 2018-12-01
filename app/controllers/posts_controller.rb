663fc7319bbc380adaa9a691751a096f129d232c
Tony Man <tonyman316@gmail.com>
2018-11-30 22:34:28 -0800

    show new post at the top

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.where(["description LIKE ?","%#{params[:search]}%"])
+    @posts = Post.where(["description LIKE ?","%#{params[:search]}%"]).order(created_at: :desc)
     @posts_find = Post.all
   end
 
